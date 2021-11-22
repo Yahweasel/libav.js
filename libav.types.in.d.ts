71b88c942eed30130590a75efb7474de3c920a3b
Yahweasel <git-rpo@yahweasel.com>
2021-11-21 20:24:14 -0500

    Adding level and profile to the codec context properties.

--- libav.types.in.d.ts
+++ libav.types.in.d.ts
@@ -197,27 +197,29 @@ export interface FilterIOSettings {
  * Supported properties of an AVCodecContext, used by ff_init_encoder.
  */
 export interface AVCodecContextProps {
-    bit_rate?: number,
-    bit_ratehi?: number,
-    channel_layout?: number,
-    channel_layouthi?: number,
-    channels?: number,
-    frame_size?: number,
-    framerate_num?: number,
-    framerate_den?: number,
-    gop_size?: number,
-    height?: number,
-    keyint_min?: number,
-    pix_fmt?: number,
-    rc_max_rate?: number,
-    rc_max_ratehi?: number,
-    rc_min_rate?: number,
-    rc_min_ratehi?: number,
-    sample_fmt?: number,
-    sample_rate?: number,
-    qmax?: number,
-    qmin?: number,
-    width?: number
+    bit_rate?: number;
+    bit_ratehi?: number;
+    channel_layout?: number;
+    channel_layouthi?: number;
+    channels?: number;
+    frame_size?: number;
+    framerate_num?: number;
+    framerate_den?: number;
+    gop_size?: number;
+    height?: number;
+    keyint_min?: number;
+    level?: number;
+    pix_fmt?: number;
+    profile?: number;
+    rc_max_rate?: number;
+    rc_max_ratehi?: number;
+    rc_min_rate?: number;
+    rc_min_ratehi?: number;
+    sample_fmt?: number;
+    sample_rate?: number;
+    qmax?: number;
+    qmin?: number;
+    width?: number;
 }
 
 export interface LibAV {
