86c40ae67d99d63732533f9306670b86ff8155c2
Francois <francoislaberge@gmail.com>
2023-05-09 09:40:34 -0400

    Release 3.9.29

--- configs/descript-mkconfig.sh
+++ configs/descript-mkconfig.sh
@@ -2,7 +2,7 @@
 ./mkconfig.js 'rawvideo' '["format-ogg", "format-webm", "codec-libopus", "format-mp4", "codec-aac", "codec-flac", "swscale", "libvpx", "decoder-libvpx_vp8", "decoder-h264", "decoder-hevc", "decoder-prores", "workerfs", "cli", "format-rawvideo", "codec-rawvideo"]'
 
 FRAGS='[
-"audio-filters", "swscale", "cli",
+"audio-filters", "swscale", "workerfs", "cli",
 "format-wav", "demuxer-mp3", "decoder-mp3", "format-mp4", "format-ogg", "format-aac",
 "demuxer-gif",
 "codec-aac", "format-pcm_f32le", "codec-pcm_f32le", "codec-libopus", "decoder-h264", "decoder-hevc", "decoder-prores"'
