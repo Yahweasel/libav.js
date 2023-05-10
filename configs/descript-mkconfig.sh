e65e34d4875147441f3753c12ebae8bec2f140c7
Francois <francoislaberge@gmail.com>
2023-05-09 21:10:23 -0400

    Adds demuxer for webm and flac

--- configs/descript-mkconfig.sh
+++ configs/descript-mkconfig.sh
@@ -8,6 +8,7 @@ FRAGS='[
 "codec-aac", "format-pcm_f32le", "codec-pcm_f32le", "codec-libopus", "decoder-h264", "decoder-hevc", "decoder-prores"'
 ./mkconfig.js 'descript-p1' "$FRAGS]"
 FRAGS="$FRAGS"',
+"demuxer-webm", "demuxer-flac",
 "codec-flac", "demuxer-aiff",
 "decoder-alac", "codec-pcm_s32le",
 "libvpx", "decoder-libvpx_vp8", "decoder-libvpx_vp9"'
