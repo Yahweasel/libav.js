95a23d712519adaecb76f653e55df0ecd4355d6b
Yahweasel <git-rpo@yahweasel.com>
2023-05-30 09:15:21 -0400

    Adding a test for ffprobe failing

--- tests/tests/100-ffmpeg-cli.js
+++ tests/tests/100-ffmpeg-cli.js
@@ -1,5 +1,7 @@
 /* Test that the CLI is working, from a high level. Actually, since we used the
  * CLI to do all our initial conversion, just checks all the files that
  * 002-formats made. */
-for (const file of h.files)
-    await h.utils.compareAudio("bbb.mp4", file.name);
+for (const file of h.files) {
+    if (/^bbb/.test(file))
+        await h.utils.compareAudio("bbb.mp4", file.name);
+}
