fd2538a2d3322685c9be52f5ef25a96aef9fb8b2
Yahweasel <git-rpo@yahweasel.com>
2024-10-17 18:12:40 -0400

    Adding the libav.js version to the license header too

--- tools/license-header.sh
+++ tools/license-header.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a very simple tool that gets the given license header and appends the
 # emcc version for documentation
-cat "$1"
+printf '/*!\n * libav.js %s\n' "$1"
+cat "$2"
 emcc --version | head -n 1 | sed 's/^/ * /'
 printf ' *\n */\n'
 cat
