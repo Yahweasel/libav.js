a01aaa88476b453dc950463af783890595417269
Yahweasel <git-rpo@yahweasel.com>
2025-08-04 16:35:30 -0400

    Adding high-level BSF functions. Resolves #85

---
+++ src/p-avbsf.in.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2019-2025 Yahweasel and contributors
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Bitstream-filter some number of packets.
+ * @param bsf  AVBSFContext(s), input
+ * @param pktPtr  AVPacket
+ * @param inPackets  Input packets
+ * @param config  Options. May be "true" to indicate end of stream.
+ */
+/* @types
+ * ff_bsf_multi@sync(
+ *     bsf: number, pktPtr: number, inPackets: (Packet | number)[],
+ *     config?: boolean | { fin?: boolean }
+ * )
+ */
+var ff_bsf_multi = Module.ff_bsf_multi = function(bsf, pktPtr, inPackets, config) {
+    var outPackets = [];
+    var transfer = [];
+
+    if (typeof config === "boolean") {
+        config = {fin: config};
+    } else if (typeof config === "undefined") {
+        config = {};
+    }
+
+    // Choose a packet copier
+    var copyoutPacket = config.copyoutPacket || ff_copyout_packet;
+
+    function handlePacket(inPacket) {
+        var ret;
+        if (inPacket !== null) {
+            ff_copyin_packet(pktPtr, inPacket);
+            ret = av_bsf_send_packet(bsf, pktPtr);
+        } else {
+            ret = av_bsf_flush(bsf);
+        }
+        if (ret < 0)
+            throw new Error("Error while feeding bitstream filter: " + ff_error(ret));
+        av_packet_unref(pktPtr);
+
+        while (true) {
+            ret = av_bsf_receive_packet(bsf, pktPtr);
+            if (ret === -6 /* EAGAIN */ || ret === -0x20464f45 /* AVERROR_EOF */)
+                break;
+            if (ret < 0)
+                throw new Error("Error while receiving a packet from the bitstream filter: " + ff_error(ret));
+
+            var outPacket = copyoutPacket(pktPtr);
+
+            if (outPacket && outPacket.libavjsTransfer && outPacket.libavjsTransfer.length)
+                transfer.push.apply(transfer, outPacket.libavjsTransfer);
+            outPackets.push(outPacket);
+            av_packet_unref(pktPtr);
+        }
+    }
+
+    // Handle each packet
+    for (var pi = 0; pi < inPackets.length; pi++) {
+        var inPacket = inPackets[pi];
+        handlePacket(inPacket);
+    }
+
+    if (config.fin)
+        handlePacket(null);
+
+    outPackets.libavjsTransfer = transfer;
+    return outPackets;
+};
---
+++ tests/tests/627-bsf.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023-2025 Yahweasel and contributors
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+// Null bitstream filter using our meta API
+
+const libav = await h.LibAV();
+
+const [fmt_ctx, streams] = await libav.ff_init_demuxer_file("bbb.webm");
+let streamIdx = -1;
+for (let i = 0; i < streams.length; i++) {
+    if (streams[i].codec_type === libav.AVMEDIA_TYPE_AUDIO) {
+        streamIdx = i;
+        break;
+    }
+}
+if (streamIdx < 0)
+    throw new Error("Could not find audio track");
+const stream = streams[streamIdx];
+
+const [, c, pkt, frame] = await libav.ff_init_decoder(
+    "libopus", streams[streamIdx].codecpar);
+// Force floating point decoding
+await libav.AVCodecContext_sample_fmt_s(c, libav.AV_SAMPLE_FMT_FLT);
+const [res, packets] = await libav.ff_read_frame_multi(fmt_ctx, pkt);
+if (res !== libav.AVERROR_EOF)
+    throw new Error("Failed to read packets");
+await libav.avformat_close_input_js(fmt_ctx);
+
+if (!packets[streamIdx].length)
+    throw new Error("No packets found for the appropriate stream");
+
+// BSF
+const bsf = await libav.av_bsf_list_parse_str_js("null");
+const outPackets = await libav.ff_bsf_multi(bsf, pkt, packets[streamIdx], true);
+if (outPackets.length !== packets[streamIdx].length)
+    throw new Error("Packets not passed thru null BSF");
+await libav.av_bsf_free_js(bsf);
+
+// Decode them
+const frames = await libav.ff_decode_multi(c, pkt, frame, outPackets, true);
+
+// Free the decoder
+await libav.ff_free_decoder(c, pkt, frame);
+
+// Check for correctness
+await h.utils.compareAudio("bbb.webm", frames);
